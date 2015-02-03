082cc9478449ec44a2d77e51a48187ad9fdcd636
Thomas Gentry <thomas.gentry@answernet.com>
2015-02-03 16:18:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Destroy All Software - Building RSpec From Scratch
---
+++ test_test_runner.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+
+class TestTestRunner < Minitest::Test
+  def test_that_it_can_pass
+  end
+
+  def test_that_it_can_fail
+  end
+end
